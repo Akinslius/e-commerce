e958f1163c84545b2286b66371d12abeab7b344f
Akinslius <akinyemi.akinwande@yahoo.com>
2022-05-24 10:57:30 +0100

    first commit

---
+++ admin/index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin dashboard</title>
+
+ <!-- CSS only -->
+ <link rel="stylesheet" href="../style.css">      
+<!-- bootstrap css link -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+
+
+</head>
+<body>
+    <div class="container-fluid p-0">
+    <nav class="navbar navbar-expand-lg navbar-light bg-info">
+        <div class="container-fluid">
+    <a class="navbar-brand" href="#"><img src="../img/logo/logo1.jpg" class="logo"></a>
+    <nav class="navbar navbar-expand-lg">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        Welcome Guest
+                    </a>
+                </li>
+            </ul>
+
+
+            </nav>
+
+            </div>
+    </nav>
+       
+        
+
+       
+    </div>
+
+
+
+
+
+
+<!--bootstrap js link  -->
+<!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>    
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,243 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ecommerce website</title>
+
+    
+    <!-- CSS only -->
+<link rel="stylesheet" href="style.css">  
+<!-- bootstrap css link -->  
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+
+    <!-- font awesome link -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+
+</head>
+<body>
+<!-- navbar -->
+<div class="container-fluid p-0">
+<nav class="navbar navbar-expand-lg navbar-light bg-info">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#"><img src="img/logo/logo1.jpg" class="logo"></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Product</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Register</a> 
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contact</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#"> <i class="fa-solid fa-cart-shopping"></i><sup>1</sup> </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Total Price:100/-</a>
+        </li>
+       
+      </ul>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-light" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+
+<!-- second child -->
+<nav class="navbar navbar-expand-lg navbar-dark bg-secondary">
+    <ul class="navbar-nav me-auto">
+    <li class="nav-item">
+          <a class="nav-link" href="#">Welcome Guest</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Login  </a>
+        </li>
+        
+
+    </ul>
+
+</nav>
+<!-- third child -->
+<div class="bg-light">
+    <h3 class="text-center">
+        Marv Organix</h3>
+        <p class="text-center">
+            Skincare and Beauty Expert
+        </p>
+
+</div>
+<!-- fourth child -->
+<div class="row">
+    <div class="col-md-10">
+        <!-- Display all product -->
+        <div class="row">
+            <div class="col-md-4 mb-2">
+            <div class="card">
+  <img src="img/product/pic1.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-info">Add to cart</a>
+    <a href="#" class="btn btn-secondary">View more</a>
+  </div>
+</div>
+
+                </div>
+            <div class="col-md-4 mb-2">
+            <div class="card">
+  <img src="img/product/pic2.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-info">Add to cart</a>
+    <a href="#" class="btn btn-secondary">View more</a>
+  </div>
+</div>
+                
+                </div>
+
+                <div class="col-md-4 mb-2">
+            <div class="card">
+  <img src="img/product/pic2.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-info">Add to cart</a>
+    <a href="#" class="btn btn-secondary">View more</a>
+  </div>
+</div>
+                
+                </div>
+    
+            <div class="col-md-4 mb-2">
+            <div class="card">
+  <img src="img/product/pic3.jpg" class="card-img-top" >
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-info">Add to cart</a>
+    <a href="#" class="btn btn-secondary">View more</a>
+  </div>
+</div>
+                
+                </div>
+
+                <div class="col-md-4 mb-2">
+            <div class="card">
+  <img src="img/product/img-1.jpg" class="card-img-top" >
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-info">Add to cart</a>
+    <a href="#" class="btn btn-secondary">View more</a>
+  </div>
+</div>
+                
+                </div>
+
+                <div class="col-md-4 mb-2">
+            <div class="card">
+  <img src="img/product/img-2.jpg" class="card-img-top" >
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-info">Add to cart</a>
+    <a href="#" class="btn btn-secondary">View more</a>
+  </div>
+</div>
+                
+                </div>
+
+        </div>
+ 
+    </div>
+    <div class="col-md-2 bg-secondary p-0">
+      <!-- Brands to be displayed -->
+      <ul class="navbar-nav me-auto text-center">
+         <li class="nav-item bg-info">
+            <a href="#" class="nav-link text-light ">
+              <h4>Delivery Brands</h4>
+            </a>
+         </li>
+         <li class="nav-item">
+            <a href="#" class="nav-link text-light ">
+              Brand 1
+            </a>
+         </li>
+         <li class="nav-item">
+            <a href="#" class="nav-link text-light ">
+              Brand 2
+            </a>
+         </li>
+         <li class="nav-item">
+            <a href="#" class="nav-link text-light ">
+              Brand 3
+            </a>
+         </li>
+         <li class="nav-item">
+            <a href="#" class="nav-link text-light ">
+              Brand 4
+            </a>
+         </li>
+      </ul>
+
+      <!-- categories to be displayed -->
+      <ul class="navbar-nav me-auto text-center">
+         <li class="nav-item bg-info">
+            <a href="#" class="nav-link text-light ">
+              <h4>Categories</h4>
+            </a>
+         </li>
+         <li class="nav-item">
+            <a href="#" class="nav-link text-light ">
+              category 1
+            </a>
+         </li>
+         <li class="nav-item">
+            <a href="#" class="nav-link text-light ">
+              category 2
+            </a>
+         </li>
+         <li class="nav-item">
+            <a href="#" class="nav-link text-light ">
+              category 3
+            </a>
+         </li>
+         <li class="nav-item">
+            <a href="#" class="nav-link text-light ">
+              category 4
+            </a>
+         </li>
+      </ul>
+        <!-- side nav -->
+
+    </div>
+</div>
+
+<!-- last child -->
+
+<div class="bg-info p-3 text-center">
+    <p>All rights reserved. Designed by <a href="https://akinslius.netlify.app//" class="footer-link">Asatech Hub</a></p>
+</div>
+
+</div>
+
+
+<!--bootstrap js link  -->
+<!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+</body>
+</html>
