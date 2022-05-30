7def57ce02d3bcb25a60fbd880074070c372f75c
Akinslius <akinyemi.akinwande@yahoo.com>
2022-05-30 08:56:00 +0100

    insert category php code

--- admin/insert_categories.php
+++ admin/insert_categories.php
@@ -1,21 +1,41 @@
 <?php
-include ('../include/connect.php')
+include ('../include/connect.php');
+
+if(isset($_POST['insert_cat'])){
+  $category = $_POST['cat_title'];
+
+// select data from db
+$select = "SELECT * FROM `category` WHERE `category_title`='$category' ";
+$res = mysqli_query($con,$select);
+$num = mysqli_num_rows($res);
+if($num > 0){
+  echo " <script>alert('This category is present')</script>";
+}else{
+  $sql = "INSERT INTO `category`(`category_title`) VALUES ('$category')";
+$result = mysqli_query($con,$sql);
+if($result){
+  echo " <script>alert('Category successfully added')</script>";
+}else{
+  echo " <script>alert('Failed to add category added')</script>";
+}
+
+}
+}
+
 
 ?>
 
 <form action="#" method="post">
 <div class="input-group w-90 mb-2">
   <span class="input-group-text bg-info" id="basic-addon1"><i class="fa-solid fa-receipt"></i></span>
-  <input type="text" class="form-control" name:"cat_title" placeholder="Insert Categories" aria-label="categories" aria-describedby="basic-addon1">
+  <input type="text" class="form-control" name="cat_title" placeholder="Insert Categories" aria-label="categories" required>
 </div>
 
 <div class="input-group w-10 mb-2 m-auto">
-    <!-- <input type="submit" class="form-control bg-info " name="insert_cat" value="Insert categories"> -->
+    <input type="submit" class="bg-info p-2 my-3 border-0" name="insert_cat" value="Insert categories">
 
-    <button class="bg-info p-2 my-3 border-0">
-    Insert categories
+    <!-- <button class="bg-info p-2 my-3 border-0" type="submit" > Insert categories </button> -->
 
-    </button>
     <!-- <div class="input-group w-90 mb-2">
   <span class="input-group-text bg-info" id="basic-addon1"><i class="fa-solid fa-receipt"></i></span>
   <input type="text" class="form-control" name:"cat_title" placeholder="Insert Categories" aria-label="Username" aria-describedby="basic-addon1">
